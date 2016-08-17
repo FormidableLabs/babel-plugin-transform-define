b36c19475486b638534aa26cc16b204fb8f5d906
Eric Baer <me@ericbaer.com>
2016-08-16 18:35:50 -0700

    Fix eslint errors

--- modules/index.js
+++ modules/index.js
@@ -4,61 +4,64 @@ export default function ({ types: t }) {
 
       // process.env.NODE_ENV
       MemberExpression(path, state) {
-        const replacements = state.opts
-        const keys = Object.keys(replacements)
+        const replacements = state.opts;
+        const keys = Object.keys(replacements);
 
         for (let i = 0, len = keys.length; i < len; ++i) {
-          const key = keys[i]
+          const key = keys[i];
 
           if (path.matchesPattern(key)) {
-            path.replaceWith(t.valueToNode(replacements[key]))
+            path.replaceWith(t.valueToNode(replacements[key]));
 
             if (path.parentPath.isBinaryExpression()) {
-              const result = path.parentPath.evaluate()
+              const result = path.parentPath.evaluate();
 
-              if (result.confident)
-                path.parentPath.replaceWith(t.valueToNode(result.value))
+              if (result.confident) {
+                path.parentPath.replaceWith(t.valueToNode(result.value));
+              }
             }
 
-            break
+            break;
           }
         }
       },
 
       // typeof window
       UnaryExpression(path, state) {
-        if (path.node.operator !== 'typeof')
-          return
+        if (path.node.operator !== "typeof") {
+          return;
+        }
+        const replacements = state.opts;
+        const keys = Object.keys(replacements);
+        const typeofValues = {};
 
-        const replacements = state.opts
-        const keys = Object.keys(replacements)
-        const typeofValues = {}
+        keys.forEach((key) => {
+          if (key.substring(0, 7) === "typeof ") {
+            typeofValues[key.substring(7)] = replacements[key];
+          }
+        });
 
-        keys.forEach(function (key) {
-          if (key.substring(0, 7) === 'typeof ')
-            typeofValues[key.substring(7)] = replacements[key]
-        })
-
-        const argumentNames = Object.keys(typeofValues)
+        const argumentNames = Object.keys(typeofValues);
 
         for (let i = 0, len = argumentNames.length; i < len; ++i) {
-          const argumentName = argumentNames[i]
+          const argumentName = argumentNames[i];
 
           if (path.node.argument.name === argumentName) {
-            path.replaceWith(t.valueToNode(typeofValues[argumentName]))
+            path.replaceWith(t.valueToNode(typeofValues[argumentName]));
 
             if (path.parentPath.isBinaryExpression()) {
-              const result = path.parentPath.evaluate()
+              const result = path.parentPath.evaluate();
 
-              if (result.confident)
-                path.parentPath.replaceWith(t.valueToNode(result.value))
+              if (result.confident) {
+                path.parentPath.replaceWith(t.valueToNode(result.value));
+              }
             }
 
-            break
+            break;
           }
         }
       }
 
     }
-  }
+  };
 }
