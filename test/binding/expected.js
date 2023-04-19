3012530ee7829fee191e6e0a169293de0901da09
inottn <inottn@outlook.com>
2023-04-19 22:44:25 +0800

    fix: should not transform identifiers with binding

---
+++ test/binding/expected.js
@@ -0,0 +1,18 @@
+"use strict";
+
+"replaced";
+
+var VAR_BINDING = "replaced";
+
+var LET_BINDING = "replaced";
+
+var CONST_BINDING = "replaced";
+
+function HOISTED_BINDING(PARAM_BINDING) {
+  "replaced";
+  VAR_BINDING;
+  LET_BINDING;
+  CONST_BINDING;
+  HOISTED_BINDING;
+  PARAM_BINDING;
+}
